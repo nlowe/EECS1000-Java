48b98a4401a12be4fd3c216cc9474831e0f75ba0
Nathan Lowe <techwiz96@gmail.com>
2014-11-20 10:25:16 -0500

    Add first example with eclipse

---
+++ FirstProgram/src/first.java
@@ -0,0 +1,7 @@
+//"This is my first program in Eclipse" (not really, but that's what the script tells me to put...)
+public class first {
+	public static void main(String[] args){
+		System.out.println("My First Eclipse Program!");
+		System.err.println("IntelliJ IDEA is better. Also, this brace format sucks...");
+	}
+}
